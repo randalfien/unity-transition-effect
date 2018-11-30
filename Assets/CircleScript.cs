d5a1c0d3d1e27f0db23a80042e6d8b3cfeca31ff
Ondřej Paška <on.paska@gmail.com>
2018-11-30 21:36:15 +0100

    Transition effects wave and circle
    Does not include Dotween

---
+++ Assets/CircleScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleScript : MonoBehaviour {
+	private void Awake()
+	{
+		GenerateCircle(1,30);
+	}
+
+	void GenerateCircle(float radius, int n)
+	{
+		Mesh mesh = new Mesh();
+		//verticies
+		List<Vector3> verticiesList = new List<Vector3>();
+		float x;
+		float y;
+		for (int i = 0; i < n; i++)
+		{
+			x = radius * Mathf.Sin((2 * Mathf.PI * i) / n);
+			y = radius * Mathf.Cos((2 * Mathf.PI * i) / n);
+			verticiesList.Add(new Vector3(x, y, 0f));
+		}
+
+		//triangles
+		List<int> trianglesList = new List<int>();
+		for (int i = 0; i < n - 2; i++)
+		{
+			trianglesList.Add(0);
+			trianglesList.Add(i + 1);
+			trianglesList.Add(i + 2);
+		}
+
+		mesh.vertices = verticiesList.ToArray();
+		mesh.triangles = trianglesList.ToArray();
+		
+		MeshFilter meshFilter = GetComponent<MeshFilter>();
+		meshFilter.mesh = mesh;
+	}
+}
---
+++ Assets/SwitchScript.cs
@@ -0,0 +1,72 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class SwitchScript : MonoBehaviour {
+
+	public GameObject Root1;
+	public GameObject Root2;
+
+	public GameObject Wiper;
+
+	public Camera Camera1;
+	public Camera Camera2;
+
+	private bool _toggled;
+	private bool _transitionActive;
+
+	public bool Circle = false;
+	private void Awake()
+	{
+		SetupCameras();
+		
+		Root1.SetActive(true);
+		Root2.SetActive(false);
+	}
+
+	private void SetupCameras()
+	{
+		Camera1.clearFlags = CameraClearFlags.SolidColor;
+		Camera1.depth = 0;
+		Camera1.cullingMask = 1 << Root1.layer;
+		
+		Camera2.clearFlags = CameraClearFlags.Nothing;
+		Camera2.depth = 1;
+		Camera2.cullingMask = 1 << Root2.layer;
+	}
+
+	private void Update()
+	{
+		if (!_transitionActive && Input.GetKeyDown(KeyCode.Space))
+		{
+			ToggleTransition();
+		}
+	}
+
+	private void ToggleTransition()
+	{
+		_toggled = !_toggled;	
+		
+		Root1.SetActive(true);
+		Root2.SetActive(true);
+		if (Circle)
+		{
+			var targetScale = !_toggled ? 11f : 0.001f;
+			var startScale = !_toggled ? 0.001f : 11f;
+			Wiper.transform.localScale = new Vector3(startScale,startScale,startScale);
+			Wiper.transform.DOScale(targetScale, 1.5f).OnComplete(OnTransitionFinished);
+		}
+		else
+		{
+			Wiper.transform.position = new Vector3(_toggled ? 0 : -31, 0, -9f);
+			Wiper.transform.DOMoveX(_toggled ? -31 : 0, 1.5f).OnComplete(OnTransitionFinished);
+		}
+		_transitionActive = true;
+	}
+
+	private void OnTransitionFinished()
+	{
+		_transitionActive = false;
+		Root1.SetActive(!_toggled);
+		Root2.SetActive(_toggled);
+	}
+}
